840fe34b0b38ab071cde27414f4ca855235fd2cb
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-09-07 01:17:34 +0300

    update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,32 @@
 // В качестве бэкенда используй публичный API сервиса Pixabay
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '39216416-ade7d4bf60cd1f5e815f694cd';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const params = new URLSearchParams({
-  key: API_KEY,
-  q: '',
+  key: '39216416-ade7d4bf60cd1f5e815f694cd',
+  q: valueSearch,
   image_type: 'photo',
   orientation: 'horizontal',
-  safesearch: true,
-  per_page: 40,
-  page: 1,
+  safesearch: 'true',
+  min_width: '360',
+  min_height: '240',
+  per_page: '40',
+  page: currentPage,
 });
 
-export async function fetchImage() {
-  const response = await fetch(`${BASE_URL}?${params}`);
-  if (!response.ok) {
-    throw new Error(response.status);
+async function fetchImage(currentPage, valueSearch) {
+  try {const response = await axios.get(`?${params}`);
+    if (response.data.total === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    return response.data;
+  } catch (error) {
+    Notify.failure(error.message);
   }
-  return await response.json();
 }
 
+export { fetchImage };
