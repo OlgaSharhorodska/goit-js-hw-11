32efdf3bf380e1567d999c006b9c73ab9d79626f
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-09-07 00:54:13 +0300

    elements.js

--- src/js/elements.js
+++ src/js/elements.js
@@ -1,5 +1,6 @@
 export const elements = {
   searchForm: document.querySelector('.search-form'),
+  input: document.querySelector('.input'),
   gallery: document.querySelector('.gallery'),
   loader: document.querySelector('.loader'),
 };
